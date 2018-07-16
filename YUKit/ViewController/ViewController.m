190378b26728145f92effaf2ce68989450512ca9
yuyanyu <yuyanyu@haodf.com>
2018-07-16 16:50:14 +0800

    Revert "Revert "pod""
    This reverts commit 6fe92e090cdc48e91c514152a0c121b7878ea07f.

--- YUKit/ViewController/ViewController.m
+++ YUKit/ViewController/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YUKitColor.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = HexColor(0xFF0000);
 }
 
 
